28c354094bdd7f858649d0b76c71aa8d36532dba
chandan jain <Chandan.Jain@ocean.ibm.com>
2022-08-12 15:19:15 +0530

    add import for msdsn

--- auth_windows.go
+++ auth_windows.go
@@ -3,6 +3,8 @@
 
 package mssql
 
+import "github.com/microsoft/go-mssqldb/msdsn"
+
 func getAuthN(p msdsn.Config) (auth auth, authOk bool) {
 	auth, authOk = getAuth(p.User, p.Password, p.ServerSPN, p.Workstation)
 	return
